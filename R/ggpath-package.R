0972a03905d1f6c6d899cef6e3e95534c6ae3d86
Sebastian Carl <mrcaseb@gmail.com>
2024-08-15 16:46:45 +0200

    make a conditional import

--- R/ggpath-package.R
+++ R/ggpath-package.R
@@ -5,5 +5,10 @@
 #' @importFrom ggplot2 element_grob
 #' @import grid
 #' @importFrom rlang %||%
+#' @rawNamespace if (getRversion() >= "4.0.0") {
+#'   importFrom(tools, R_user_dir)
+#' } else {
+#'   importFrom(backports, R_user_dir)
+#' }
 ## usethis namespace: end
 NULL
