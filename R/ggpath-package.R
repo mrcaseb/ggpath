7fd1a37aecb6bb6336e560dc2f9c3c3ce9eb7b6c
mrcaseb <mrcaseb@gmail.com>
2025-03-31 15:42:35 +0200

    Require R4.1 and drop backports dependency

--- R/ggpath-package.R
+++ R/ggpath-package.R
@@ -5,10 +5,6 @@
 #' @importFrom ggplot2 element_grob
 #' @import grid
 #' @importFrom rlang %||%
-#' @rawNamespace if (getRversion() >= "4.0.0") {
-#'   importFrom(tools, R_user_dir)
-#' } else {
-#'   importFrom(backports, R_user_dir)
-#' }
+#' @importFrom tools R_user_dir
 ## usethis namespace: end
 NULL
